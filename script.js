5a5446a157e87af37424db53ddfc2f4af980d3cb
g-akca <gok.akca66@gmail.com>
2026-03-09 02:49:22 +0300

    Add attributes for accessibility

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
 function validateInput(input) {
-    const error = input.closest(".input-div, .textarea-div, fieldset").querySelector(".error-text");
+    const error = document.getElementById(input.getAttribute("aria-describedby"));
 
     if (input.type == "email" && (input.value.trim() == "" || !input.validity.valid)) {
         error.hidden = false;
+        input.setAttribute("aria-invalid", "true");
         input.classList.add("error");
         return;
     }
 
     if ((input.type == "text" || input.tagName == "TEXTAREA") && input.value.trim() == "") {
         error.hidden = false;
+        input.setAttribute("aria-invalid", "true");
         input.classList.add("error");
         return;
     }
 
     if (input.type == "checkbox" && !input.checked) {
         error.hidden = false;
+        input.setAttribute("aria-invalid", "true");
         return;
     }
 
     error.hidden = true;
     input.classList.remove("error");
+    input.setAttribute("aria-invalid", "false");
 }
 
 const form = document.querySelector("form");
 const inputs = form.querySelectorAll("input:not([type='radio']), textarea");
 
-inputs.forEach(input => {
-    input.addEventListener("input", () => validateInput(input));
-});
+inputs.forEach(input => input.addEventListener("input", () => validateInput(input)));
+
+const radios = form.querySelectorAll("input[type='radio']");
+const radioError = document.getElementById(radios[0].getAttribute("aria-describedby"));
+
+radios.forEach(radio => radio.addEventListener("change", () => {
+    radioError.hidden = true;
+    radios.forEach(r => r.setAttribute("aria-invalid", "false"));
+}));
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     inputs.forEach(input => validateInput(input));
 
-    const radios = form.querySelectorAll("input[type='radio']");
-    const radioError = form.querySelector(".query-div .error-text");
-
     const selected = [...radios].some(radio => radio.checked);
-    radioError.hidden = selected;
 
-    radios.forEach(radio => radio.addEventListener("change", () => radioError.hidden = true));
+    if (!selected) {
+        radioError.hidden = false;
+        radios.forEach(radio => radio.setAttribute("aria-invalid", "true"));
+    }
+    else {
+        radioError.hidden = true;
+        radios.forEach(radio => radio.setAttribute("aria-invalid", "false"));
+    }
 });
